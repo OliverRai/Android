849035e6395a4bdfb43e831ba20d1258c4fa166b
OliverRai <raiane.mateus.oliveira@gmail.com>
2019-10-01 15:15:22 -0300

    primeiro commit, activity splash

---
+++ app/src/main/java/com/petsapp/splash.java
@@ -0,0 +1,30 @@
+package com.petsapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarLogin();
+            }
+        }, 2000);
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(splash.this,
+                Login.class);
+        startActivity(intent);
+        finish();
+    }
+}
